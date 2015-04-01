45333b05858b3031c6f646cdc9e2973e0d15a1bd
Parker Won <pwon111@gmail.com>
2015-04-01 15:00:31 -0700

    updated style.css

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config={};
 
-config.home="/Users/mpierce/GeoGateway/";
+config.home="/home/pwon111/Documents/repos/geogateway/";
 config.bin=config.home+"/bin/";
 config.baseDestDir="html/userUploads/";
 
