50db81e5b02aad32c3f6383ca1631711c63ccd15
marpierc <marpierc@iu.edu>
2015-04-16 16:37:50 -0400

    Removing config.js and replacing it with sample-config.js. You must manually create a local copy of config.js from the sample. The README has been updated. config.js has been placed in .gitignore, so it can't be accidentally checked in.

--- config.js
+++
@@ -1,8 +0,0 @@
-var config={};
-
-config.home="/home/webgisdeveloper/Bitbucket/geogateway/";
-//config.home="~/Bitbucket/geogateway/";
-config.bin=config.home+"/bin/";
-config.baseDestDir="html/userUploads/";
-
-module.exports=config;
