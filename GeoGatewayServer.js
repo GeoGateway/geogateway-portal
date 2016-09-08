6fb0d9a038c97ccd6082bdee1448fa267515910b
Tuku Man <wang208@indiana.edu>
2016-09-08 11:40:34 -0400

    remove image name from LOS csv

--- GeoGatewayServer.js
+++ GeoGatewayServer.js
@@ -629,7 +629,7 @@ app.get('/los_query/',function(req,res) {
         data=data.replace(regex,'');
         res.setHeader('Content-Type','text/csv');
         res.setHeader('Content-Disposition','attachment; filename="'+image_uid+'".csv"');
-        data = "image name, "+image_name+"\n"
+        data = image_name+"\n"
             +"start, "+lat1+", "+lng1+"\n"
             +"end, "+lat2+", "+lng2+"\n"
             +"azimuth, "+azimuth+"\n"
