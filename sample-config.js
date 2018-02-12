cf8ec42f37c857ddef1723c7af9182959e8e7787
Tuku Man <wang208@indiana.edu>
2018-02-12 15:43:08 -0500

    add seismicity plot url

--- sample-config.js
+++ sample-config.js
@@ -12,5 +12,6 @@ config.wmscolorUrl = 'http://149.165.157.180/geoserver/InSAR/wms?';
 config.sldserviceUrl = "http://149.165.157.180/insar";
 config.uavsarratingUrl = "http://gf2.ucs.indiana.edu/quaketables/uavsar/rating?";
 config.gpsserviceUrl = "http://156.56.177.220:8000/gpsservice/kml?";
+config.seismicityplotUrl = "http://gf8.ucs.indiana.edu:8000/seismicityservice/plot?"
 
 module.exports=config;
