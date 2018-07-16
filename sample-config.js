eb4ae070d14135f1d770b1fc316792de85dff756
Tuku Man <wang208@indiana.edu>
2018-07-16 18:07:55 -0400

    #43 test rapid los tools on all data set

--- sample-config.js
+++ sample-config.js
@@ -7,7 +7,7 @@ config.baseDestDir="html/userUploads/";
 config.uavsarSearchUrl='http://gf2.ucs.indiana.edu/quaketables/uavsar/search?';
 config.wmsUrl='http://149.165.157.180/geoserver/highres/wms?';
 config.losQueryUrl='http://gf1.ucs.indiana.edu/insartool/profile?image=InSAR:uid';
-config.altlosQueryUrl='http://156.56.177.220:5000/los/profile?image=uid';
+config.altlosQueryUrl='http://gf8.ucs.indiana.edu:5000/los/profile?image=uid';
 config.wmscolorUrl = 'http://149.165.157.180/geoserver/InSAR/wms?';
 config.sldserviceUrl = "http://149.165.157.180/insar";
 config.uavsarratingUrl = "http://gf2.ucs.indiana.edu/quaketables/uavsar/rating?";
