11d71cfa68ba482953762b284a13f03f5ddf1594
Tuku Man <wang208@indiana.edu>
2020-12-07 11:58:42 -0500

    updated config ...

--- sample-config.js
+++ sample-config.js
@@ -1,17 +1,23 @@
 var config={};
 
-config.home="/Users/mpierce/GeoGateway/";
+config.home="/home/webgisdeveloper/github/geogateway-portal/";
 config.bin=config.home+"/bin/";
 config.baseDestDir="html/userUploads/";
 
-config.uavsarSearchUrl='http://gf2.ucs.indiana.edu/quaketables/uavsar/search?';
-config.wmsUrl='http://149.165.157.180/geoserver/highres/wms?';
+config.wmsUrl='http://js-157-39.jetstream-cloud.org/geoserver/highres/wms?';
 config.losQueryUrl='http://gf1.ucs.indiana.edu/insartool/profile?image=InSAR:uid';
-config.altlosQueryUrl='http://gf8.ucs.indiana.edu:5000/los/profile?image=uid';
-config.wmscolorUrl = 'http://149.165.157.180/geoserver/InSAR/wms?';
-config.sldserviceUrl = "http://149.165.157.180/insar";
+//config.altlosQueryUrl='http://156.56.177.220:5000/los/profile?image=uid';
+config.altlosQueryUrl='http://js-157-58.jetstream-cloud.org:8000/los/profile?image=uid';
+config.wmscolorUrl = 'http://js-157-39.jetstream-cloud.org/geoserver/InSAR/wms?';
+//config.sldserviceUrl = "http://149.165.168.89/insar";
+config.sldserviceUrl = "http://js-157-39.jetstream-cloud.org/insar";
+
+//config.uavsarSearchUrl='http://localhost/quaketables/uavsar/search?';
+//config.uavsarratingUrl = "http://localhost/quaketables/uavsar/rating?";
+config.uavsarSearchUrl='http://gf2.ucs.indiana.edu/quaketables/uavsar/search?';
 config.uavsarratingUrl = "http://gf2.ucs.indiana.edu/quaketables/uavsar/rating?";
-config.gpsserviceUrl = "http://156.56.177.220:8000/gpsservice/kml?";
+config.gpsserviceUrl = "http://156.56.174.162:8000/gpsservice/kml?";
 config.seismicityplotUrl = "http://gf8.ucs.indiana.edu:8000/seismicityservice/plot?"
+
 
 module.exports=config;
